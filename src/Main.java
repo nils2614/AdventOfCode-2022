6328d13d0390ce1390a75870c78148283d912360
nilsoffermann <nils@offermann.net>
2023-02-02 09:37:00 +0100

    Add Tag

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         try {

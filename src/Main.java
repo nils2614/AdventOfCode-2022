23328d78f702a2403c2b7f66190e0654facdfea3
nilsoffermann <nils@offermann.net>
2023-02-02 13:51:53 +0100

    Day 3, Part 1 working

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,47 @@ public class Main {
         try {
             File myInput = new File("./src/input.txt");
             Scanner myScan = new Scanner(myInput);
+            int totalPriority = 0;
 
             while (myScan.hasNextLine()) {
                 String line = myScan.nextLine();
-                //code here
+                totalPriority += priorityOfDublicate(line);
+                //System.out.println(findDublicate(line)); // Test the findDublicate() method
             }
+            System.out.println("The sum of the priorities of the dublicates is: " + totalPriority);
+            //System.out.println(priorityOfDublicate("CrZsJsPPZsGzwwsLwLmpwMDw")); // Test with any given string
 
         } catch (FileNotFoundException e) {
             System.out.println("error");
+        }
+    }
+
+    public static char findDublicate(String line) {
+        int compartmentSize = line.length() / 2;
+        char dublicate;
+
+        for (int i = 0; i < compartmentSize; i++) {
+            dublicate = line.charAt(i);
+            for (int j = compartmentSize; j < compartmentSize*2; j++) {
+                if (dublicate == line.charAt(j)) {
+                    return dublicate;
+                }
+            }
+        }
+        return ' ';
+    }
+
+    public static int priorityOfDublicate(String line) {
+        int asciiValue = findDublicate(line);
+
+        if ((asciiValue > 96) && (asciiValue < 123)) { // test for lowercase letter
+            return asciiValue - 96;
+        }
+        else if ((asciiValue > 64) && (asciiValue < 91)) { // test for uppercase letter
+            return asciiValue - 38;
+        }
+        else {
+            return -1;
         }
     }
 }

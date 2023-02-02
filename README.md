2e25eced4b75a99ca93544d37794f8f76fa9fe63
nilsoffermann <nils@offermann.net>
2023-02-02 13:03:41 +0100

    Clear everything for Day 3

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# Advent of Code - Day 2
+# Advent of Code - Day 3
 
-Day 2 of Advent of Code by Nils Offermann
-
-#### Scores
-A - Rock \
-B - Paper \
-C - Scissors
-
-X - Rock \
-Y - Paper \
-Z - Scissors
-
-AX -> 1+3 = 4 \
-AY -> 2+6 = 8 \
-AZ -> 3+0 = 3 \
-BX -> 1+0 = 1 \
-BY -> 2+3 = 5 \
-BZ -> 3+6 = 9 \
-CX -> 1+6 = 7 \
-CY -> 2+0 = 2 \
-CZ -> 3+3 = 6
-
-#### Part 2
-X - loose \
-Y - draw \
-Z - win
+Day 3 of Advent of Code by Nils Offermann
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-// Day 2 of Advent of Code by Nils Offermann
+// Day 3 of Advent of Code by Nils Offermann
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -8,106 +8,14 @@ public class Main {
         try {
             File myInput = new File("./src/input.txt");
             Scanner myScan = new Scanner(myInput);
-            int totalScore = 0;
 
             while (myScan.hasNextLine()) {
                 String line = myScan.nextLine();
-                //totalScore = totalScore + calcScore(line); //use for Part 1
-                totalScore = totalScore + calcOutcome(line); //use for Part 2
-                //System.out.println(line.charAt(0) + "" + line.charAt(2) + ": " + calcScore(line)); //debug part for Part 1
+                //code here
             }
-            System.out.println(totalScore);
 
         } catch (FileNotFoundException e) {
             System.out.println("error");
-        }
-    }
-
-    public static int calcScore(String line) {
-        char c1 = line.charAt(0);
-        char c2 = line.charAt(2);
-        if (c1 == 'A') {
-            switch(c2) {
-                case 'X':
-                    return 4;
-                case 'Y':
-                    return 8;
-                case 'Z':
-                    return 3;
-                default:
-                    return -1;
-            }
-        }
-        else if (c1 == 'B') {
-            switch(c2) {
-                case 'X':
-                    return 1;
-                case 'Y':
-                    return 5;
-                case 'Z':
-                    return 9;
-                default:
-                    return -1;
-            }
-        }
-        else if (c1 == 'C') {
-            switch(c2) {
-                case 'X':
-                    return 7;
-                case 'Y':
-                    return 2;
-                case 'Z':
-                    return 6;
-                default:
-                    return -1;
-            }
-        }
-        else {
-            return -1;
-        }
-    }
-
-    public static int calcOutcome(String line) {
-        char c1 = line.charAt(0);
-        char c2 = line.charAt(2);
-        if (c1 == 'A') {
-            switch(c2) {
-                case 'X':
-                    return 3;
-                case 'Y':
-                    return 1 + 3;
-                case 'Z':
-                    return 2 + 6;
-                default:
-                    return -1;
-            }
-        }
-        else if (c1 == 'B') {
-            switch(c2) {
-                case 'X':
-                    return 1;
-                case 'Y':
-                    return 2 + 3;
-                case 'Z':
-                    return 3 + 6;
-                default:
-                    return -1;
-            }
-        }
-        else if (c1 == 'C') {
-            switch(c2) {
-                case 'X':
-                    return 2;
-                case 'Y':
-                    return 3 + 3;
-                case 'Z':
-                    return 1 + 6;
-                default:
-                    return -1;
-            }
-        }
-        else {
-            return -1;
         }
     }
 }

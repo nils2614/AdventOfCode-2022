33809bbdaefd3b5066da60bd17bb4c317608b91a
Nils Offermann <nils@offermann.net>
2023-02-13 14:09:34 +0100

    Implement two base functions for day 5

--- src/main.go
+++ src/main.go
@@ -9,12 +9,13 @@ import (
 )
 
 func main() {
-	var test []string = fileToArray("./src/input.txt")
-	fmt.Println(test[0])
+	var inputSlice []string = fileToSlice("./src/input.txt")
+	var myStacks [][]string = initializeStacks(inputSlice, 9, 8)
+	fmt.Println(myStacks[2])
 }
 
-func fileToArray(input string) []string {
-	file, err := os.Open(input)
+func fileToSlice(inputFile string) []string {
+	file, err := os.Open(inputFile)
 
 	if err != nil {
 		log.Fatalf("failed to open")
@@ -30,4 +31,21 @@ func fileToArray(input string) []string {
 
 	file.Close()
 	return text
+}
+
+func initializeStacks(inputSlice []string, width int, height int) [][]string {
+	stackArray := make([][]string, width, width)
+
+	for i := 0; i < width; i++ {
+		stackArray[i] = make([]string, height, 50)
+		for j := height - 1; j >= 0; j-- {
+			myChar := string(inputSlice[j][1+i*4])
+			//fmt.Println("i:", i, "j:", j, "c:", myChar)
+			if myChar != " " {
+				stackArray[i][height-j-1] = myChar
+			}
+		}
+	}
+
+	return stackArray
 }
